13df5af961f3a1cce50a0a9f70bb3c992a9817d7
Kamran Khan <kamrankhan@Kamrans-MacBook-Air.local>
2024-12-14 23:44:30 +0530

    added a toolbar for editor

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -4,10 +4,47 @@ import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import TaskItem from '@tiptap/extension-task-item'
 import TaskList from '@tiptap/extension-task-list'
+import Image from '@tiptap/extension-image'
+import ImageResize from 'tiptap-extension-resize-image'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import Underline from '@tiptap/extension-underline'
+import FontFamily from '@tiptap/extension-font-family'
+import TextStyle from '@tiptap/extension-text-style'
+import { useEditorStore } from '@/store/use-editor-store'
+
 
 export const Editor = () => {
+//zustand store
+ const {setEditor} = useEditorStore();
 
   const editor = useEditor({
+    onCreate({editor}) {
+        setEditor(editor) 
+    },
+    onDestroy() {
+        setEditor(null)  
+    },
+    onUpdate({editor}) {
+        setEditor(editor) 
+    },
+    onSelectionUpdate({editor}) {
+        setEditor(editor) 
+    },
+    onFocus({editor}) {
+        setEditor(editor) 
+    },
+    onTransaction({editor}) {
+        setEditor(editor) 
+    },
+    onBlur({editor}) {
+        setEditor(editor) 
+    },
+    onContentError({editor}) {
+        setEditor(editor) 
+    },
     editorProps:{
       attributes:{
         style: "padding-left:56px; padding-right:56px;",
@@ -16,12 +53,38 @@ export const Editor = () => {
     },
     extensions: [
         StarterKit,
+        Image,
+        ImageResize,
         TaskList,
         TaskItem.configure({
           nested: true,
          }),
+         Table.configure({
+            resizable: true,
+          }),
+          TableRow,
+          TableHeader,
+          TableCell,
+          Underline,
+          FontFamily,
+          TextStyle
     ],
-    content: '<p>Hello! 🌎️</p>',
+    content: `
+    <table>
+      <tbody>
+        <tr>
+          <th>Name</th>
+          <th colspan="3">Description</th>
+        </tr>
+        <tr>
+          <td>Cyndi Lauper</td>
+          <td>Singer</td>
+          <td>Songwriter</td>
+          <td>Actress</td>
+        </tr>
+      </tbody>
+    </table>
+  `,
   })
 
   return(
