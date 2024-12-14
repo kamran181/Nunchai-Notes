88b75a2cf010f06361827d651ff525c9f6adea71
Kamran Khan <kamrankhan@Kamrans-MacBook-Air.local>
2024-12-14 14:25:59 +0530

    added tiptap edito

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from '@/components/ui/button'
+import Link from 'next/link';
 import React from 'react'
 
 const Home = () => {
   return (
-    <div className='flex min-h-screen items-center justify-center bg-pink-500'>
-      <Button size={'lg'}>Click Me</Button>
+    <div className='flex min-h-screen items-center justify-center'>
+    <h5>Please <Link href={'/documents/234'}><span className='text-blue-500 underline'>Click Here</span></Link> to go to documents id page</h5>
     </div>
   )
 }
