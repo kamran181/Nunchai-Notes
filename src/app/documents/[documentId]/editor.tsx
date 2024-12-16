2f768a83935198ac724c612971beebcfd6ab5c1a
Kamran Khan <kamrankhan@Kamrans-MacBook-Air.local>
2024-12-16 10:02:53 +0530

    added many functionaltied like image , highlight

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -7,12 +7,16 @@ import TaskList from '@tiptap/extension-task-list'
 import Image from '@tiptap/extension-image'
 import ImageResize from 'tiptap-extension-resize-image'
 import Table from '@tiptap/extension-table'
+import Link from '@tiptap/extension-link'
 import TableCell from '@tiptap/extension-table-cell'
 import TableHeader from '@tiptap/extension-table-header'
 import TableRow from '@tiptap/extension-table-row'
 import Underline from '@tiptap/extension-underline'
 import FontFamily from '@tiptap/extension-font-family'
 import TextStyle from '@tiptap/extension-text-style'
+import { Color } from '@tiptap/extension-color'
+import Highlight from '@tiptap/extension-highlight'
+import TextAlign from '@tiptap/extension-text-align'
 import { useEditorStore } from '@/store/use-editor-store'
 
 
@@ -21,6 +25,7 @@ export const Editor = () => {
  const {setEditor} = useEditorStore();
 
   const editor = useEditor({
+    autofocus:true,
     onCreate({editor}) {
         setEditor(editor) 
     },
@@ -53,6 +58,11 @@ export const Editor = () => {
     },
     extensions: [
         StarterKit,
+        Link.configure({
+            openOnClick: false,
+            autolink: true,
+            defaultProtocol: 'https'
+        }),
         Image,
         ImageResize,
         TaskList,
@@ -67,7 +77,12 @@ export const Editor = () => {
           TableCell,
           Underline,
           FontFamily,
-          TextStyle
+          TextStyle,
+          Highlight.configure({ multicolor: true }),
+          Color,
+          TextAlign.configure({
+            types: ['heading', 'paragraph'],
+          })
     ],
     content: `
     <table>
--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,11 +1,355 @@
 "use client"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+
+import { useState } from "react";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
 import { useEditorStore } from "@/store/use-editor-store";
+import {type Level} from "@tiptap/extension-heading";
+import { type ColorResult , SketchPicker} from "react-color"
 
-import { BoldIcon, ChevronDownIcon, ItalicIcon, ListTodoIcon, LucideIcon, MessageSquarePlusIcon, PrinterIcon, Redo2Icon, RemoveFormattingIcon, SpellCheckIcon, UnderlineIcon, Undo2Icon } from "lucide-react";
+import { AlignLeftIcon, AlignRightIcon,AlignCenterIcon,AlignJustifyIcon,BoldIcon, ChevronDownIcon, HighlighterIcon, ImageIcon, ItalicIcon, Link2Icon, ListTodoIcon, LucideIcon, MessageSquarePlusIcon, PrinterIcon, Redo2Icon, RemoveFormattingIcon, SearchIcon, SpellCheckIcon, UnderlineIcon, Undo2Icon, UploadIcon, ListIcon, ListOrderedIcon } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 
+const ListButton = ()=>{
+    const {editor} = useEditorStore();
+
+    const lists =[
+        {
+            label:"Bullet List" , 
+            icon : ListIcon ,
+            onClick:()=> editor?.chain().focus().toggleBulletList().run(),
+            isActive:editor?.isActive("bulletList")
+        },
+        {
+            label:"Ordered List" , 
+            icon : ListOrderedIcon ,
+            onClick:()=> editor?.chain().focus().toggleOrderedList().run(),
+            isActive:editor?.isActive("orderedList")
+        }
+
+    ]
+
+    return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+          <button className=" min-w-7 shrink-0 flex  items-center justify-between rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm ">
+           <ListIcon className="size-4" />   
+         </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+            {lists.map(({ label, icon:Icon , onClick , isActive})=>(
+               <button
+                key={label}
+                onClick={onClick}
+                className={cn(
+                    "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+                    isActive && "hover:bg-neutral-200/80"
+                 )} 
+               >
+                <Icon className="size-4" /> 
+                <span className="text-sm">{label}</span>
+               </button>
+            ))}
+
+          </DropdownMenuContent>
+            
+        </DropdownMenu> 
+    )
+
+}
+
+const AlignButton = ()=>{
+    const {editor} = useEditorStore();
+
+    const alignments =[
+        {label:"Align Left" , value :"left" , icon :AlignLeftIcon},
+        {label:"Align Right" , value :"right" , icon :AlignRightIcon},
+        {label:"Align Center" , value :"center" , icon :AlignCenterIcon},
+        {label:"Align Justify" , value :"justify" , icon :AlignJustifyIcon},
+
+    ]
+
+    return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+          <button className=" min-w-7 shrink-0 flex  items-center justify-between rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm ">
+           <AlignLeftIcon className="size-4" />   
+         </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+            {alignments.map(({ label, value, icon:Icon})=>(
+               <button
+                key={value}
+                onClick={()=> editor?.chain().focus().setTextAlign(value).run()}
+                className={cn(
+                    "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+                    editor?.isActive({textAlign: value}) && "hover:bg-neutral-200/80"
+                 )} 
+               >
+                <Icon className="size-4" /> 
+                <span className="text-sm">{label}</span>
+               </button>
+            ))}
+
+          </DropdownMenuContent>
+            
+        </DropdownMenu> 
+    )
+
+}
+
+const ImageButton = ()=>{
+    const {editor} = useEditorStore();
+    const [isDialogOpen , setIsDialogopen] = useState(false);
+    const [imageUrl , setImageUrl] = useState("")
+
+    const onChange = (src: string)=>{
+        editor?.chain().focus().setImage({src}).run();
+    }
+
+    const onImageUpload = ()=>{
+        // Dynamically create an input element
+        const input = document.createElement("input");
+        input.type = "file";
+        input.accept = "image/*"; // Specify accepted file types (e.g., images only)
+        
+        // Attach an event listener to handle the file selection
+        input.onchange = (event: Event) => {
+        const target = event.target as HTMLInputElement;
+        if (target.files && target.files[0]) {
+            const file = target.files[0];
+            const imageUrl = URL.createObjectURL(file)
+            onChange(imageUrl)
+        }
+        };
+
+        // Programmatically trigger the click event to open the file picker
+        input.click();
+    }
+
+    const handleImageUrlSubmit = ()=>{
+        if(imageUrl){
+            onChange(imageUrl)
+            setImageUrl("");
+            setIsDialogopen(false)
+        }
+    };
+
+
+
+    return (
+        <>
+          <DropdownMenu >
+          <DropdownMenuTrigger asChild>
+          <button className=" min-w-7 shrink-0 flex  items-center justify-between rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm ">
+           <ImageIcon className="size-4" />   
+         </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent >
+            <DropdownMenuItem onClick={onImageUpload}>
+                <UploadIcon className="size-4 mr-2"/>
+                 Upload
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={()=> setIsDialogopen(true)}>
+                <SearchIcon className="size-4 mr-2"/>
+                 Paste Image Url
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+            
+        </DropdownMenu> 
+
+        <Dialog open={isDialogOpen} onOpenChange={setIsDialogopen}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        Insert Image Url
+                    </DialogTitle>
+                </DialogHeader>
+                <Input 
+                  placeholder="Insert Image Url"
+                  value={imageUrl}
+                  onChange={e=>setImageUrl(e.target.value)}
+                  onKeyDown={(e)=>{
+                    if(e.key =='Enter'){
+                        handleImageUrlSubmit() 
+                    }
+                  }}
+                />
+
+            <DialogFooter>
+                <Button 
+                  onClick={handleImageUrlSubmit}
+                >
+                    Insert
+                </Button>
+            </DialogFooter>
+
+            </DialogContent>
+            
+
+        </Dialog>
+        </>
+       
+    )
+
+}
+
+const LinkButton = ()=>{
+    const {editor} = useEditorStore();
+    const [value, setValue] = useState<string>("");
+
+    const onChange = (href: string)=>{
+        editor?.chain().focus().extendMarkRange('link').setLink({href}).run();
+        setValue("")
+    }
+
+    return (
+        <DropdownMenu onOpenChange={(open)=>{
+            if(open){
+                setValue(editor?.getAttributes("link").href || "")
+            }
+        }}>
+          <DropdownMenuTrigger asChild>
+          <button className=" min-w-7 shrink-0 flex  items-center justify-between rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm ">
+           <Link2Icon className="size-4" />   
+         </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="p-2.5 flex items-center gap-x-2">
+            <Input
+             placeholder="https://example.com"
+             value={value}
+             onChange={e => setValue(e.target.value)}
+            />
+            <Button 
+             onClick={()=> onChange(value)}
+            >
+                Apply
+            </Button>
+          </DropdownMenuContent>
+            
+        </DropdownMenu> 
+    )
+
+}
+
+const HighlightColorButton = ()=>{
+    const {editor} = useEditorStore();
+
+    const value = editor?.getAttributes("highlight").color || "#FFFFFF"
+     const onChange=(color:ColorResult)=>{
+        editor?.chain().focus().setHighlight({color : color.hex}).run();
+    }
+
+    return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+          <button className=" min-w-7 shrink-0 flex  items-center justify-between rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm ">
+           <HighlighterIcon className="size-4" />   
+         </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="p-0">
+            <SketchPicker
+              onChange={onChange}
+              color={value}
+              />
+          </DropdownMenuContent>
+            
+        </DropdownMenu> 
+    )
+
+}
+
+const TextColorButton = ()=>{
+    const {editor} = useEditorStore();
+
+    const value = editor?.getAttributes("textStyle").color || "#000000"
+     const onChange=(color:ColorResult)=>{
+        editor?.chain().focus().setColor(color.hex).run();
+    }
+
+    return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+          <button className=" min-w-7 shrink-0 flex flex-col items-center justify-between rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm ">
+
+            <span className="text-xs">A</span>
+            <div className="h-0.5 w-full" style={{backgroundColor:value}} />
+         </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="p-0">
+            <SketchPicker
+              onChange={onChange}
+              color={value}
+              />
+          </DropdownMenuContent>
+            
+        </DropdownMenu> 
+    )
+
+}
+
+const HeadingLevelButton =()=>{
+    const {editor} = useEditorStore();
+
+    const headings = [
+        {label:"Normal Text" , value : 0 , fontSize:"16px"},
+        {label:"Heading 1" , value : 1 , fontSize:"32px"},
+        {label:"Heading 2" , value : 2 , fontSize:"24px"},
+        {label:"Heading 3" , value : 3 , fontSize:"20px"},
+        {label:"Heading 4" , value : 4 , fontSize:"18px"}
+
+    ]
+
+    const getCurrentHeadings =()=>{
+        for(let level =1 ; level <= 5 ; level++){
+            if(editor?.isActive("heading" ,{level})){
+                return `Heading ${level}`
+            }
+        }
+        return "Normal Text"
+    };
+
+    return(
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+              <button className="h-7 min-w-7 shrink-0 flex items-center justify-between rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm ">
+                  <span className="truncate">
+                     {getCurrentHeadings()}
+                  </span>
+                  <ChevronDownIcon className="ml-2 size-4 shrink-0" />
+              </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="p-1 flex flex-col gap-y-1">
+            {headings.map(({label , value ,fontSize})=>(
+                <button 
+                 key={value}
+                 style={{
+                    fontSize,
+                }}
+                
+                 onClick={()=>{
+                    if(value ===0){
+                        editor?.chain().focus().setParagraph().run();
+                    }else{
+                        editor?.chain().focus().toggleHeading({level: value as Level}).run(); 
+                    }
+                 }} 
+                 className={cn(
+                    "flex items-center gap-x-2 px-2 py-1 rounded-sm hover:bg-neutral-200/80",
+                    (value === 0 && !editor?.isActive("heading")) || editor?.isActive("heading" , {level : value}) && "bg-neutral-200/80"
+                 )}                
+                 >
+                 {label}
+                </button>
+            ))}
+        </DropdownMenuContent>
+          </DropdownMenu>
+    );
+
+}
 
 const FontFamilyButton = ()=>{
     const {editor} = useEditorStore();
@@ -22,7 +366,7 @@ const FontFamilyButton = ()=>{
         <DropdownMenuTrigger asChild>
             <button className="h-7 w-[125px] shrink-0 flex items-center justify-between rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm ">
                 <span className="truncate">
-                   {editor?.getAttributes("textStyle").FontFamily || "Arial"}
+                   {editor?.getAttributes("textStyle").fontFamily || "Arial"}
                 </span>
                 <ChevronDownIcon className="ml-2 size-4 shrink-0" />
             </button>
@@ -159,14 +503,36 @@ export const Toolbar = ()=>{
 
     <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
 
-    {sections[1].map((item)=>(
-        <ToolbarButton key={item.label} {...item}/>
-    ))}
-    <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
-
     <FontFamilyButton/>
 
     <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
+
+    <HeadingLevelButton/>
+
+    <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
+
+
+    {sections[1].map((item)=>(
+        <ToolbarButton key={item.label} {...item}/>
+    ))}
+
+    <TextColorButton/>
+
+    <HighlightColorButton/>
+    
+
+    <Separator orientation="vertical" className="h-6 bg-neutral-300"/>
+
+    <LinkButton/>
+
+    <ImageButton/>
+
+    <AlignButton/>
+
+    {/* Line height */}
+
+    <ListButton/>
+
 
     {sections[2].map((item)=>(
         <ToolbarButton key={item.label} {...item}/>
