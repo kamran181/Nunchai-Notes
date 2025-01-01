2f3142df62a4f86077aa3e43245176ba9fcbf2b2
Kamran Khan <kamrankhan@Kamrans-MacBook-Air.local>
2025-01-02 00:46:29 +0530

    added ruler for editor

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -20,6 +20,8 @@ import TextAlign from '@tiptap/extension-text-align'
 import { useEditorStore } from '@/store/use-editor-store'
 import { FontSizeExtension } from '@/extensions/font-size'
 import { LineHeightExtension } from '@/extensions/line-height'
+import { Ruler } from './ruler'
+
 
 
 
@@ -28,6 +30,7 @@ export const Editor = () => {
  const {setEditor} = useEditorStore();
 
   const editor = useEditor({
+    immediatelyRender:false,
     autofocus:true,
     onCreate({editor}) {
         setEditor(editor) 
@@ -109,6 +112,7 @@ export const Editor = () => {
 
   return(
     <div className='size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible'>
+      <Ruler/>
     <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
       <EditorContent editor={editor} />
     </div>   
